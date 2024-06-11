46f0dbe1f419ffeceaff3dd86fae37c184497f04
Tanhapon Tosirikul <2781155T@student.gla.ac.uk>
2024-06-11 22:42:15 +0700

    added scripts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log("Hello World")
+import { mergeSort } from "./mergeSort";
+
+function demo() {
+    const collection1 = [1, 3, 5];
+    const collection2 = [10, 8, 6, 4, 2];
+    const collection3 = [11, 12, 13, 14, 15];
+    
+    console.log("Collection 1:", collection1);
+    console.log("Collection 2:", collection2);
+    console.log("Collection 3:", collection3);
+
+    const mergedArray = mergeSort(collection1, collection2, collection3);
+    console.log("Merged and Sorted Array:", mergedArray);
+}
+
+demo();
